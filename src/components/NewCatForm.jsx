2c96a7c1e8f703ce11ab68a0359f28012b171baa
Mikelle <mmikelle.wade@adadevelopersacademy.org>
2025-12-19 14:03:30 -0500

    adds. React: More Forms livecode changes

--- src/components/NewCatForm.jsx
+++ src/components/NewCatForm.jsx
@@ -1,30 +1,61 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const NewCatForm = ({onHandleSubmit}) => {
-    const [name, setName] = useState('');
+const kDefaultsFormState = {
+  name: '',
+  personality: '',
+  color: ''
+};
 
-    const handleNameChange = (event) => {
-        setName(event.target.value);
+const NewCatForm = ({onHandleSubmit}) => {
+    const [formData, setFormData] = useState(kDefaultsFormState);
+
+
+    const handleChange = (event) => {
+      const inputValue = event.target.value;
+      const inputName = event.target.name;
+
+      setFormData(formData => {
+        return {
+          ...formData,
+          [inputName]: inputValue
+        };
+      });
     };
 
     const handleSubmit = (event) =>
     {
         event.preventDefault();
-        const newCat = {
-            name,
-            personality: 'festive',
-            color: 'Silver and Gold',
-            petCount: 0
-        };
-        onHandleSubmit(newCat);
-        setName('');
+        onHandleSubmit(formData);
+        setFormData(kDefaultsFormState);
+    };
+
+    const makeControlledInput = (inputName) => {
+      return (
+        <input
+          type="text"
+          name={inputName}
+          id={`input-${inputName}`}
+          value={formData[inputName]}
+          onChange={handleChange}
+        />
+      );
     };
 
     return (
         <form onSubmit={handleSubmit}>
+          <div>
             <label htmlFor="name">Cat Name:</label>
-            <input type="text" id="name" name="name" value={name} onChange={handleNameChange}/>
+            { makeControlledInput('name') }
+          </div>
+          <div>
+            <label htmlFor="color">Color:</label>
+            { makeControlledInput('color') }
+          </div>
+          <div>
+            <label htmlFor="personality">Personality:</label>
+            { makeControlledInput('personality') }
+          </div>
             <div>
                 <input type="submit" value="Add a cat" />
             </div>
